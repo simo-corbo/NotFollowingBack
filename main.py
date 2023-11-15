03f8f1b7b03a6e2fe78b7cccc1109709c9b4977b
Simone <126069316+simo-corbo@users.noreply.github.com>
2023-11-15 01:20:01 +0100

    not following back and not following

--- main.py
+++ main.py
@@ -3,16 +3,33 @@ import json
 
 if __name__ == '__main__':
     # Opening JSON file
-    f = open('followers.json')
-
+    followerFile = open('followers_1.json')
+    followingFile =open('following.json')
     # returns JSON object as
     # a dictionary
-    data = json.load(f)
-
+    dataFollower = json.load(followerFile)
+    dataFollowing = json.load(followingFile)
     # Iterating through the json
-    # list
-    for i in data['emp_details']:
-        print(i)
+    followers = []
+    followings =[]
 
-    # Closing file
-    f.close()
+    for i in dataFollower:
+        followers.append(i['string_list_data'][0]['value'])
+    for i in dataFollowing['relationships_following']:
+        followings.append(i['string_list_data'][0]['value'])
+
+    print("Follower: ", len(followers))
+    print("Following: ", len(followings))
+    followerFile.close()
+    followingFile.close()
+    followers.sort()
+    followings.sort()
+    print("Not following me back: ")
+    for following in followings:
+        if following not in followers:
+            print(following)
+    print("\n -------------------\n")
+    print("I don't follow back: ")
+    for follower in followers:
+        if follower not in followings:
+            print(follower)
